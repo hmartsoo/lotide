ec3c2da88e13301f878ea63d5e2ec67a96c60e34
Helen Martinez <michelina.martinez@gmail.com>
2022-05-22 22:47:26 -0400

    intro exercise for console.assert

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+/*
+Our assertion function assertEqual is a better fit for our needs than console.assert.
+Ours prints out a colourful and therefore easy to read message in both success and failure scenarios.
+*/
