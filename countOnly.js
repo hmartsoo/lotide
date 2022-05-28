0da54eb7c77e434d1f0dc32fb151610a99f906e5
hmartsoo <michelina.martinez@gmail.com>
2022-05-28 17:59:18 -0400

    Count only the items that we are looking for and input them into a new Object list

--- countOnly.js
+++ countOnly.js
@@ -8,11 +8,21 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  results = {};
+
+  //The results object is empty right now, but we will use it to store our count of each item we want to find
+  const results = {};
+
+  //Use a for of loop to look through all the items
   for (const item of allItems) {
+
+    //if the item we want to count is true then...
     if (itemsToCount[item]) {
-      if(results[item]) {
+
+      //...if we already counted this item before, we add that item to our count
+      if (results[item]) {
         results[item] += 1;
+
+      //but if we haven't counted this item before that we want, we give it a value of 1
       } else {
         results[item] = 1;
       }
